e21bcc8e362f390a72706a618924be5e29c026de
Vikash <vikasha@vsoftconsulting.com>
2018-03-08 15:48:03 +0530

    Added preloading strategy for all modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes} from '@angular/router';
 import { WatchListComponent } from './watch-list/watch-list.component';
 import { HomeComponent } from './core/home/home.component';
 import { GenreComponent } from './genre/genre.component';
@@ -13,7 +13,7 @@ const appRoutes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
+  imports: [RouterModule.forRoot(appRoutes, {preloadingStrategy: PreloadAllModules})],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
